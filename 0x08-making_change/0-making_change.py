c1f3332f8b4aa087ffc3736d77aab889a2845c99
Aughostinos <augustinos.nabil@gmail.com>
2024-10-04 04:59:37 +0300

    fix effiecieny problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,12 +9,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
+    coins = [coin for coin in set(coins) if coin <= total]
+    if not coins:
+        return -1
+
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for amount in range(coin, total + 1):
-            if dp[amount - coin] != float('inf'):
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+            if dp[amount - coin] + 1 < dp[amount]:
+                dp[amount] = dp[amount - coin] + 1
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
